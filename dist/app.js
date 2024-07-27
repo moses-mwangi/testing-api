09b22ebbd9214d2b86712e79ccdf01091eb60063
moses mwangi <moses.mwangi.me@gmail.com>
2024-07-27 14:25:19 +0300

    h

---
+++ dist/app.js
@@ -0,0 +1,37 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const users_1 = __importDefault(require("./routes/users"));
+const app = (0, express_1.default)();
+const allowedOrigins = [
+    "http://localhost:3000",
+    "http://localhost:3002",
+    "https://food-delivery-bkrk.vercel.app",
+    "https://food-delivery-dasboard-pk5j.vercel.app",
+    "https://food-delivery-dasboard.vercel.app",
+];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+};
+app.use((0, cors_1.default)(corsOptions));
+app.use(express_1.default.json());
+app.use((0, cookie_parser_1.default)());
+app.use((req, res, next) => {
+    console.log("Testing middleware");
+    next();
+});
+app.use("/api/users", users_1.default);
+exports.default = app;
---
+++ dist/server.js
@@ -0,0 +1,27 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const app_1 = __importDefault(require("./app"));
+const env = process.env.NODE_ENV || "development";
+// dotenv.config({ path: `./.env.${env}` });
+dotenv_1.default.config({ path: "config.env" });
+process.on("uncaughtException", (err) => {
+    console.error("UNHANDLED EXCEPTION ---- Shutting down 💥");
+    console.error(err.name, err.stack, err.message);
+    process.exit(1);
+});
+const db = "mongodb+srv://mosesmwangime:4Owf3JAY8YpVbT0y@fooddelivercluster.q3ihtsr.mongodb.net/delivery?retryWrites=true&w=majority&appName=FoodDeliverCluster";
+mongoose_1.default
+    .connect(db)
+    .then(() => {
+    console.log("Database has succefully connneccted");
+})
+    .catch((err) => console.error(err, "moess"));
+const port = Number(process.env.PORT) || 3005;
+const server = app_1.default.listen(port, "127.0.0.1", () => {
+    console.log(`listening to port ${port}`);
+});
