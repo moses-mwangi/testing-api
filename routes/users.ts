613d7d431ac2e998ba98ec22e111f0dbcb02f8ad
moses mwangi <moses.mwangi.me@gmail.com>
2024-07-27 15:57:24 +0300

    j

--- routes/users.ts
+++ routes/users.ts
@@ -1,8 +1,14 @@
 import express, { Router } from "express";
+import Order from "../models/orderModel";
 
 const router: Router = express.Router();
 
-router.route("/").get((req, res, next) => {
+router.route("/").get(async (req, res, next) => {
+  const order = await Order.find();
+
+  if (order)
+    return res.status(200).json({ status: "success", data: { data: "no" } });
+
   res.status(200).json({
     status: "success",
     data: "moses mwangi",
